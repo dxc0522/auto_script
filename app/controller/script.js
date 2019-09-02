7ce66a45df50433ae619b90c7d269eeaa24c0321
dxc0522 <809685229@qq.com>
2019-09-02 17:24:29 +0800

    init

---
+++ app/controller/script.js
@@ -0,0 +1,31 @@
+"use strict";
+const process = require("child_process");
+const Controller = require("egg").Controller;
+class ScriptController extends Controller {
+  async blog() {
+    const { ctx } = this;
+    const result = await new Promise((resolve, reject) => {
+      process.exec(
+        "cd /home/my_space_formal && git fetch --all && git reset --hard origin/master && git pull",
+        function(error) {
+          resolve(error);
+        }
+      );
+    });
+    ctx.body = result ? result : "博客代码已更新";
+  }
+  async pwd() {
+    const { ctx } = this;
+    const result = await new Promise((resolve, reject) => {
+      process.exec(
+        "cd /home/my_pwd_serve && yarn stop && yarn start",
+        function(error) {
+          resolve(error);
+        }
+      );
+    });
+    ctx.body = result ? result : "我的密码代码已更新";
+  }
+}
+
+module.exports = ScriptController;
