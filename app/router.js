6f7c212ae544d3848642eae2963c49a8a04585b6
dxc0522 <809685229@qq.com>
2019-09-02 17:38:17 +0800

    change post

--- app/router.js
+++ app/router.js
@@ -7,5 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/blog', controller.script.blog);
-  router.get('/pwd', controller.script.pwd);
+  router.post('/pwd', controller.script.pwd);
 };
