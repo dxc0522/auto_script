7673d52e9ee1469fc51702d2956fb0499234f8c4
dxc0522 <809685229@qq.com>
2019-09-02 17:58:09 +0800

    change cxrf

--- config/config.default.js
+++ config/config.default.js
@@ -23,7 +23,14 @@ module.exports = appInfo => {
 
   // add your middleware config here
   config.middleware = [];
-
+  // 关闭安全协议
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+    }
+    // domainWhiteList: ["http://127.0.0.1:8080"]
+  };
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
